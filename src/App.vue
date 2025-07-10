06db92ae0d4f80ceddf7e209eb6922ba39edb366
Joe <36142847+swyuliang@users.noreply.github.com>
2025-07-10 20:44:09 +0800

    V2学习todolist案例

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="root">
+  <h1>ToDoList</h1>
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <MyHeader :addTodo="addTodo"/>
+      <MyList :todos="todos" :CheckTodo="CheckTodo"/>
+      <MyFooter/>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader'
+import MyList from './components/MyList'
+import MyFooter from './components/MyFooter'
 
 export default {
   name: 'App',
+  data() {
+    return {
+        todos:[
+          {id:'001', title:'吃饭', done:true},
+          {id:'002', title:'睡觉', done:true},
+          {id:'003', title:'打豆豆', done:true}
+        ]
+    }
+  },
   components: {
-    HelloWorld
+    MyHeader,
+    MyList,
+    MyFooter
+  },
+  methods:{
+    addTodo(todoObj) {
+      this.todos.unshift(todoObj)
+    },
+    CheckTodo(id){
+      this.todos.forEach((todo)=>{
+        if(todo.id === id) todo.done = !todo.done
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+h1 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+/*base*/
+body {
+  background: #fff;
+}
+
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
 </style>
---
+++ src/components/MyList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="todo-main">
+    <MyItem v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj" :CheckTodo="CheckTodo"/>
+  </ul>
+</template>
+<script>
+import MyItem from './MyItem'
+export default {
+  name: 'My-List',
+  components: {
+    MyItem
+  },
+  props:['todos','CheckTodo']
+}
+</script>
+<style>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
