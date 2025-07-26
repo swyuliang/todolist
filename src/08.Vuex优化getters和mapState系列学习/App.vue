27e2dfabaed67a4a391d06a6de867adf68039ff9
Joe <36142847+swyuliang@users.noreply.github.com>
2025-07-26 14:31:54 +0800

    Vuex优化getter和mapState系列方法学习

---
+++ src/08.Vuex优化getters和mapState系列学习/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="root">
+  <PageCount></PageCount>
+</div>
+</template>
+
+<script>
+import PageCount from './components/PageCount'
+export default {
+  name: 'App',
+  data(){
+    return {}
+  },
+  components: { PageCount },
+  mounted(){},
+  methods:{},
+  watch: {}
+}
+</script>
+
+<style scoped>
+
+</style>
